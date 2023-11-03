d77ef51b5f3b2d6f44999b8d79ee6ba8acdad62c
Alistair <s2051131@ed.ac.uk>
2023-11-03 19:23:46 +0000

    Camera Edits on Raspberry Pi

--- src/Localisation/camera.py
+++ src/Localisation/camera.py
@@ -3,6 +3,7 @@
 # Import robot camera
 from picamera import PiCamera
 from collections import deque
+from time import sleep
 import numpy as np
 
 
@@ -16,16 +17,16 @@ class Camera:
     def get_image(self, i):
         return self.images[i]
 
-    def capture_image(self):
+    def capture_image(self, output_dir):
         # Create a new PiCamera object
         camera = PiCamera()
 
         # Set the resolution of the camera
-        camera.resolution = (4056, 3040)
+        camera.resolution = (1920, 1080)
 
         # Capture an image
-        image = np.empty((4056, 3040, 3), dtype=np.uint8)
-        camera.capture(image, "rgb")
+        image = np.empty((1920, 1080, 3), dtype=np.uint8)
+        camera.capture(output_dir)
 
         # Clean up the camera resources
         camera.close()
@@ -33,3 +34,14 @@ class Camera:
         # Return the captured image as a numpy array
         self.images.appendleft(image)
         return image
+    
+    def save_image(self, image, name):
+        image.save(name)
+
+
+camera = Camera()
+sleep(2)
+for i in range(10):
+    camera.capture_image("photo"+str(i)+".jpg")
+    sleep(0.1)
+    
