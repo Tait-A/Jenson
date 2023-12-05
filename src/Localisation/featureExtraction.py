9c490b09c6745e1813f6ea54bc5547f689fa76f3
Tait-A <s2051131@ed.ac.uk>
2023-12-05 17:13:17 +0000

    I tried some more

--- src/Localisation/featureExtraction.py
+++ src/Localisation/featureExtraction.py
@@ -2,7 +2,7 @@
 
 import cv2
 import numpy as np
-from camera import Camera
+import os
 from collections import deque
 
 # Feature Extractor class to extract features from images taken by the camera
@@ -12,31 +12,56 @@ class FeatureExtractor:
     def __init__(self, camera=None, track=None):
         self.camera = camera
         self.track = track
-        self.algorithm = cv2.SIFT_create()
+        self.algorithm = cv2.ORB_create()
         self.featureQueue = deque(maxlen=20)
+        self.path = "//Output/Features/"
 
     # Extract features from the image
-    def extractFeatures(self, img):
+    def extractFeatures(self, img, img_name):
         # Load the ceiling image
-        ceiling_image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+        greyscale_image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
 
-        # Initialize the SIFT detector
+        brightness_threshold = 200  # Adjust this threshold as needed
+        mask = greyscale_image > brightness_threshold
+
+        # Cap the brightness of the brightest pixels
+        greyscale_image[mask] = brightness_threshold
+
+        normalised_image = cv2.normalize(greyscale_image, None, 0, 255, cv2.NORM_MINMAX)
 
         # Detect keypoints and compute descriptors
-        keypoints, descriptors = self.algorithm.detectAndCompute(ceiling_image, None)
+        keypoints, descriptors = self.algorithm.detectAndCompute(normalised_image, None)
 
         # Draw keypoints on the original image
-        ceiling_image_with_keypoints = cv2.drawKeypoints(ceiling_image, keypoints, None)
+        image_with_keypoints = cv2.drawKeypoints(greyscale_image, keypoints, None)
 
         # Save the image with detected keypoints
-        cv2.imwrite("ceiling_image_with_keypoints.jpg", ceiling_image_with_keypoints)
+        img_path = img_name+"_with_keypoints.jpg"
+        print(img_path)
+        cv2.imwrite(img_path, image_with_keypoints)
 
         # Display the resulting image with detected keypoints
-        cv2.imshow("Ceiling Image with Keypoints", ceiling_image_with_keypoints)
-        cv2.waitKey(0)
+        cv2.imshow("Image with Keypoints", image_with_keypoints)
+        cv2.waitKey(2)
         cv2.destroyAllWindows()
 
         # Print the number of detected keypoints
         print("Number of keypoints detected:", len(keypoints))
 
         return keypoints, descriptors
+    
+
+    def enhance_contrast_and_whiten(image, alpha=1.5, beta=25):
+        """
+        Enhance contrast and whiten an image.
+
+        Parameters:
+        - alpha: float, contrast control (default is 1.5).
+        - beta: int, brightness control (default is 25).
+        """
+
+        # Normalize pixel values to [0, 255]
+        normalized_image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+
+        # Save the output image
+        return normalized_image
