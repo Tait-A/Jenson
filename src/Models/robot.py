216ee56512796bf81d96e97cd15ed3d7928178db
Tait-A <s2051131@ed.ac.uk>
2024-01-06 06:34:24 +0000

    model update

--- src/Models/robot.py
+++ src/Models/robot.py
@@ -7,16 +7,16 @@ import sys
 import os
 
 class Robot:
-    def __init__(self, x=0, y=0, theta=0, v=0, w=0, width=0, length=0, camera=None, track=None):
+    def __init__(self, x=0, y=0, theta=0, phi=0, v=0, w=0, width=0, length=0, steering_lims=45):
         self.x = x              # x position
         self.y = y              # y position
         self.theta = theta      # angle of robot
+        self.phi = phi          # steering angle of robot
         self.v = v              # linear velocity
         self.w = w              # angular velocity
         self.width = width      # width of robot
         self.length = length    # length of robot
-        self.camera = camera    # camera object
-        self.track = track      # track object
+        self.steering_lims = steering_lims # steering limits of robot
 
     def localise(self):
         # implementation of localisation algorithm
