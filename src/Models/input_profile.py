e12d6cbad799a5218fcd8f915ccd0c5ea46fb4c1
Tait-A <s2051131@ed.ac.uk>
2024-01-31 23:12:51 +0000

    parameterise

--- src/Models/input_profile.py
+++ src/Models/input_profile.py
@@ -2,11 +2,12 @@ import numpy as np
 from scipy.interpolate import PPoly
 from scipy.spatial.distance import cdist
 from splines import Spline
+from robot import Robot
 
 class Profiler:
-    def __init__(self, trajectory, robot, intervals = 200):
+    def __init__(self, trajectory: Spline, robot: Robot, intervals = 200):
         self.intervals = intervals
-        self.trajectory = self.trajectory
+        self.trajectory = trajectory
         self.robot = robot
 
         self.profile()
@@ -21,5 +22,37 @@ class Profiler:
 
         nodes = trajectory.spline(ti).T
 
-        # calculate the curvature of the trajectory
-        curvature = trajectory.curvature()
+        # calculate the curvature of the trajectory and radius of curvature
+        _, radius = trajectory.calc_curvature(self.intervals)
+
+        steering_angle = np.arctan(robot.wheelbase / radius)
+        self.steering_profile = Spline(steering_angle)
+        gravity = 9.81
+
+        max_velocity = np.sqrt(robot.friction * radius * gravity) # set frictional force equal to centripetal force to get max velocity
+
+        velocity = np.minimum(robot.max_speed, max_velocity)
+
+        first_lap_velocity = velocity.copy()
+
+        first_lap_velocity[0] = max_acc
+
+        for i in range(1, len(first_lap_velocity)):
+            if first_lap_velocity[i] - first_lap_velocity[i-1] > max_acc:
+                first_lap_velocity[i] = first_lap_velocity[i-1] + max_acc
+            if first_lap_velocity[i] > max_velocity:
+                first_lap_velocity[i] = max_velocity
+
+        if velocity[0] - first_lap_velocity[-1] > max_acc:
+            velocity[0] = first_lap_velocity[-1] + max_acc:
+
+        for i in range(1, len(velocity)):
+            if velocity[i] - velocity[i-1] > max_acc:
+                velocity[i] = velocity[i-1] + max_acc
+            if velocity[i] > max_velocity:
+                velocity[i] = max_velocity
+
+
+
+
+        acceleration =
