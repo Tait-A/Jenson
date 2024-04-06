5af424e2736dd0e827a6e332c6ca1f1d5af0681e
Tait-A <s2051131@ed.ac.uk>
2024-04-06 22:04:09 +0100

    comra

--- src/Utils/cameraCalibration.py
+++ src/Utils/cameraCalibration.py
@@ -9,11 +9,14 @@ import cv2
 import numpy as np
 
 # Specify the directory path
-directory = os.path.join('/' + os.path.join(*os.path.dirname(__file__).split('/')[0:-1]), "Output/Chess")
+directory = os.path.join(
+    "/" + os.path.join(*os.path.dirname(__file__).split("/")[0:-1]),
+    "Output/calibration",
+)
 
 
 # Get the file paths of all the images in the directory
-image_paths = [os.path.join(directory, "chess"+str(i+1)+".jpg") for i in range(5)]
+image_paths = [os.path.join(directory, "chess" + str(i + 1) + ".jpg") for i in range(5)]
 
 # Read each image
 images = []
@@ -22,8 +25,7 @@ for image_path in image_paths:
     if image is not None:
         images.append(image)
 
-chessboard = (7,7)
-
+chessboard = (7, 7)
 
 
 # Creating vector to store vectors of 3D points for each checkerboard image
@@ -34,32 +36,36 @@ imgpoints = []
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((1, chessboard[0] * chessboard[1], 3), np.float32)
-objp[0,:,:2] = np.mgrid[0:chessboard[0], 0:chessboard[1]].T.reshape(-1, 2)
+objp[0, :, :2] = np.mgrid[0 : chessboard[0], 0 : chessboard[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
 for image in images:
-    retval, corners = cv2.findChessboardCorners(image, chessboard, flags = cv2.CALIB_USE_INTRINSIC_GUESS)
+    retval, corners = cv2.findChessboardCorners(
+        image, chessboard, flags=cv2.CALIB_USE_INTRINSIC_GUESS
+    )
     print(retval)
 
     if retval == True:
         objpoints.append(objp)
         # refining pixel coordinates for given 2d points.
-        corners2 = cv2.cornerSubPix(image, corners, (11,11),(-1,-1), criteria)
-         
+        corners2 = cv2.cornerSubPix(image, corners, (11, 11), (-1, -1), criteria)
+
         imgpoints.append(corners2)
- 
+
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, chessboard, corners2, retval)
-     
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
- 
-cv2.destroyAllWindows()
- 
-h,w = img.shape[:2]
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, image.shape[::-1], None, None)
- 
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+h, w = img.shape[:2]
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+    objpoints, imgpoints, image.shape[::-1], None, None
+)
+
 print("Camera matrix : \n")
 print(mtx)
 print("dist : \n")
