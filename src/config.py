6a18234ee27eb4c836eba9b44317bfb1a0b1c828
Tait-A <s2051131@ed.ac.uk>
2024-04-06 22:03:46 +0100

    conf

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ import os
 
 RPI_IPV4 = "192.168.105.135"
 HOST_IPV4 = "129.215.2.28"
-MAC_IPV4 = "192.168.105.79"
+MAC_IPV4 = "172.20.10.2"
 BROADCAST_PATH = "/Jenson/src/Utils/broadcaster.py"
 SRC_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
